b9dd67e936ecf20ff7679f3961a63e2761c51243
charles neblett <charles.neblett@charless-MacBook-Pro.local>
2020-05-18 18:00:15 -0500

    specify file

--- src/browser.tsx
+++ src/browser.tsx
@@ -1,4 +1,4 @@
-import { React, ReactDOM } from "https://unpkg.com/es-react";
+import { React, ReactDOM } from "https://unpkg.com/es-react@16.8.60/index.js";
 import { App } from "./http/get-index/app.tsx";
 
 window.addEventListener("DOMContentLoaded", () => {
